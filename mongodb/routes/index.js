d50e58af987e0977fc0a48bb2aacde9d77e33417
keerthanans <k.sudalaimani@perficient.com>
2020-08-17 20:54:25 +0530

    first commit

---
+++ mongodb/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Cool,huh!',condition:false,anyArray:[1,2,3] });
+});
+router.get('/test/:id',(req,res, next)=>{
+res.render('test',{output:req.params.id});
+});
+
+router.post('/test/submit',(req,res, next)=>{
+  var id=req.body.id;
+  res.redirect('/test/' +id);
+  });
+module.exports = router;
